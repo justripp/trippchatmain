202c42dbd055026faf93833084a4cae905791441
justripp <105530675+justripp@users.noreply.github.com>
2025-01-12 15:18:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const chessLink = "";
+const pongLink = '';
+const tetrisLink = '';
+const chatLink = 'http://tripp.chat';
+
+const chessBtn = document.getElementById("ChessBtn");
+const pongBtn = document.getElementById("PongBtn");
+const tetrisBtn = document.getElementById("TetrisBtn");
+const chatBtn = document.getElementById("ChatBtn");
+
+
+
+function chessOpen(){
+    window.open("https://chess.ngrok.pro")
+}
+
+function chatOpen(){
+    window.open("http://172.236.127.104/")
+}
